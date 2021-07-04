2099c81cb887a4f1769fe8dd97bcb278791d5353
ryancole38 <rac0029@mix.wvu.edu>
2021-07-04 15:15:25 -0400

    vertices property now a function, can scale hexagons by setting size.

--- modules/tests/tile.test.js
+++ modules/tests/tile.test.js
@@ -14,6 +14,9 @@ test('Can access vertices', () => {
 test('Adjacent vertices are one unit apart', () => {
     let tile = new Tile(0, 0);
     let verts = tile.vertices;
+
+    // all edge vertices should be 1 unit away from
+    // adjacent vertices
     for(let i=0; i<6; i++){
         expect(
             MathUtils.floatsEqual(
@@ -22,6 +25,8 @@ test('Adjacent vertices are one unit apart', () => {
             )
         ).toBe(true);
     }
+
+    // all edge vertices should be one unit away from center
     for(let i=0; i<6; i++){
         expect(
             MathUtils.floatsEqual(
@@ -32,3 +37,46 @@ test('Adjacent vertices are one unit apart', () => {
     }
 });
 
+test('Tiles at positions other than (0,0) have correct verts', () => {
+    let tile = new Tile(1, 1);
+    let center = new Tile(0, 0);
+    let verts = tile.vertices;
+    let centerVerts = center.vertices;
+
+    for(let i=0; i<6; i++){
+        expect(
+            Math.abs(verts[i].x - centerVerts[i].x)
+        ).toBe(1);
+
+        expect(
+            Math.abs(verts[i].y - centerVerts[i].y)
+        ).toBe(1);
+    }
+});
+
+test('Can set size', () => {
+    let tile = new Tile(0,0);
+    expect(tile.size).toBe(1);
+    tile.setSize(2);
+    expect(tile.size).toBe(2);
+});
+
+test('Setting size scales vertices', () => {
+    let tile = new Tile(0,0);
+    let verts = tile.vertices;
+
+    tile.setSize(2);
+
+    let scaledVerts = tile.vertices;
+
+    for(let i=0; i<6; i++){            
+
+        expect(
+            MathUtils.floatsEqual(
+                verts[i].distanceFromPoint(scaledVerts[i]),
+                1.0
+            )
+        ).toBe(true);
+
+    }
+});
--- modules/tile.js
+++ modules/tile.js
@@ -3,19 +3,22 @@ import { Point } from "./point.js"
 class Tile{
     constructor(x, y){
         this.center = new Point(x, y);
-        this.vertices = this._getVertices()
+        this.size = 1;
     }
 
-    _getVertices = () => {
-        return [
-            new Point(0.5, Math.sqrt(3)/2),
-            new Point(1,0),
-            new Point(0.5, -Math.sqrt(3)/2),
-            new Point(-0.5, -Math.sqrt(3)/2),
-            new Point(-1, 0),
-            new Point(-0.5, Math.sqrt(3)/2)
-        ];
+    setSize = (size) => {
+        this.size = size;
+    }
 
+    get vertices(){
+        return [
+            new Point(this.center.x + this.size * 0.5, this.center.y + this.size * Math.sqrt(3)/2),
+            new Point(this.center.x + this.size * 1, this.center.y + this.size * 0),
+            new Point(this.center.x + this.size * 0.5, this.center.y + this.size * -Math.sqrt(3)/2),
+            new Point(this.center.x + this.size * -0.5, this.center.y + this.size * -Math.sqrt(3)/2),
+            new Point(this.center.x + this.size * -1, this.center.y + this.size * 0),
+            new Point(this.center.x + this.size * -0.5, this.center.y + this.size * Math.sqrt(3)/2)
+        ];
     }
 }
 
